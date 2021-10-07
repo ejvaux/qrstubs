6b5ddd2b9ab29abc142b97b495a84b211954b18b
Percival <renzbondad53@gmail.com>
2021-10-07 17:37:07 +0800

    add table

---
+++ app/Http/Controllers/StubsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Stubs;
+use Illuminate\Http\Request;
+
+class StubsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Stubs  $stubs
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Stubs $stubs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Stubs  $stubs
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Stubs $stubs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Stubs  $stubs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Stubs $stubs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Stubs  $stubs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Stubs $stubs)
+    {
+        //
+    }
+}
