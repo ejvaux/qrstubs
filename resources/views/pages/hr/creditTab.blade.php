0fc812f5bf266cb9dcd2be7d95c63e0f1fdba198
EJ Mati <ejvaux_05126@yahoo.com>
2021-11-17 09:29:50 +0800

    Moved upload button to credits tab

--- resources/views/pages/hr/creditTab.blade.php
+++ resources/views/pages/hr/creditTab.blade.php
@@ -1,8 +1,14 @@
 <div id="userModalDiv"></div>
+@include('includes.modal.creditImportModal')
 <div class="row mt-2">
+    {{--<div class="col"></div>--}}
     <div class="col text-right">
+        <button id="uploadCreditsBtn" class="btn btn-outline-secondary py-1">Upload</button>
         <button id="userExportBtn" class="btn btn-outline-secondary py-1">Export</button>
     </div>
+    {{--<div class="col">
+        <button id="userExportBtn" class="btn btn-outline-secondary py-1">Export</button>
+    </div>--}}
 </div>
 <div class="row">
     <div class="col">
--- resources/views/pages/hr/empTab.blade.php
+++ resources/views/pages/hr/empTab.blade.php
@@ -1,9 +1,9 @@
-@include('includes.modal.creditImportModal')
+{{--@include('includes.modal.creditImportModal')
 <div class="row mt-2">
     <div class="col text-right">
         <button id="uploadCreditsBtn" class="btn btn-outline-secondary py-1">Upload Credits</button>
     </div>
-</div>
+</div>--}}
 <div id="hrTable">
     {{-- FUNCTION IN JS HAS AUTO RELOAD --}}
     {{-- @include('includes.table.hrTbl') --}}
