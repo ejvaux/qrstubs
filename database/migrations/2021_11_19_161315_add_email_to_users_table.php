0d7f37969709f29d53e0f9d76bf2991fe143a744
Percival <renzbondad53@gmail.com>
2021-11-23 10:56:56 +0800

    nullable email

--- database/migrations/2021_11_19_161315_add_email_to_users_table.php
+++ database/migrations/2021_11_19_161315_add_email_to_users_table.php
@@ -14,7 +14,7 @@ class AddEmailToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('email');
+            $table->string('email')->nullable();
         });
     }
 
