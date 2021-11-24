31f1fd77e3680045cc1867ed63b61c724ce69ad6
EJ Mati <ejvaux_05126@yahoo.com>
2021-11-24 11:07:31 +0800

    npm run prod. created resources canteen.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
-mix.js('public/js/canteen.js', 'public/js');
+    .sass('resources/sass/app.scss', 'public/css').sourceMaps();
+mix.js('resources/js/canteen.js', 'public/js');
 
 if (mix.inProduction()) {
     mix.version();
