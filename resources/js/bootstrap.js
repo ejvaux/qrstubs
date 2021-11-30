fe00ec59e6471f0edd2dcea330c5033744bd0a76
EJ Mati <ejvaux_05126@yahoo.com>
2021-11-30 08:27:50 +0800

    sending mail

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -44,9 +44,9 @@ if (token) {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo'
+import Echo from 'laravel-echo'
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
@@ -54,3 +54,12 @@ if (token) {
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.PUSHER_APP_KEY,
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+});
