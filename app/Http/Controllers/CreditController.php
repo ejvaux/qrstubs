35bb8051c6c73c2a8488d49f548238f666c2aad1
Percival <renzbondad53@gmail.com>
2021-12-07 10:49:13 +0800

    orderByName & '='

--- app/Http/Controllers/CreditController.php
+++ app/Http/Controllers/CreditController.php
@@ -29,7 +29,7 @@ class CreditController extends Controller
 
         $expr = $this->generateExpirationNum();
         
-        $users = $users->paginate(10);
+        $users = $users->orderBy('name')->paginate(10);
         return view('includes.table.creditTbl', compact('users','ctrl','expr'));
     }
     
