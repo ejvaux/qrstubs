20d83bc8027ac99f473da061a8b5e1d6627c1b43
EJ Mati <ejvaux_05126@yahoo.com>
2022-02-18 11:09:56 +0800

    Update: Contact Us modal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,49 +119,14 @@
                 </div>
             </div>
         </nav>
-        <div class="modal modal-primary fade" id="questionModal" tabindex="-1" data-backdrop="false" role="dialog" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1><b>Contact Information</b></h1>
-                        <button type="button" name="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p>
-                            <h3>HR Department</h3>
-                            <b>Name:</b>&nbsp; Divine Goce<br>
-                            <b>Email:</b>&nbsp; divine_goce@sercomm.com<br>
-                            <b>Extension #:</b>&nbsp; 80911<br>
-                            {{-- <br>
-                            <b>Name:</b>&nbsp; Karen Alinsod<br>
-                            <b>Email:</b>&nbsp;  karen_alinsod@sercomm.com<br>
-                            <b>Extension #:</b>&nbsp; 80919 --}}
-                            <br><br>
-                            <h3>MIS</h3>
-                            <b>Name:</b>&nbsp; Lawrence Bondad<br>
-                            <b>Email:</b>&nbsp;  lawrence_bondad@sercomm.com<br>
-                            <b>Extension #:</b>&nbsp; 80862 <br>
-                            <br>
-                            <b>Name:</b>&nbsp; Edmund Mati<br>
-                            <b>Email:</b>&nbsp; edmund_mati@sercomm.com<br>
-                            <b>Extension #:</b>&nbsp; 80861
-                        </p>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
 
-                </div>
-            </div>
-        </div>
 
         {{--End of editing --}}
 
         <main class="py-4">
             @yield('content')
         </main>
+        @include('includes.modal.contactUsModal')
     </div>
     @livewireScripts
     @stack('scripts')
